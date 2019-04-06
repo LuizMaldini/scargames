65b53e4252198cb6b393fcc702b52b714c2246dc
LuizMaldini <luiz23acdc@live.com>
2019-04-05 23:00:26 -0300

    alter excluir add abril

--- ScarGames/src/java/br/com/scargames/controller/BandeiraMB.java
+++ ScarGames/src/java/br/com/scargames/controller/BandeiraMB.java
@@ -14,26 +14,31 @@ import br.com.scargames.services.BandeiraService;
 import java.io.Serializable;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @ManagedBean(name = "bandeiraMB")
-@ViewScoped//só qdo sair dessa pag que o objeto deixa de ser criado
+@SessionScoped//só qdo sair dessa pag que o objeto deixa de ser criado
 
 
 public class BandeiraMB implements Serializable{
 
     private List<Bandeira> bandeiras;
-    private Bandeira bandeira;
-    
-    
+    private Bandeira bandeira = new Bandeira();
+
     
     public BandeiraMB() {
-        this.listar();
+       // this.listar();
     }
     
     public void listar(){
         BandeiraService service = new BandeiraService();
         bandeiras = service.listar();
+       // bandeiras.add(new Bandeira(1,"Visa"));
+       // bandeiras.add(new Bandeira(2,"Master"));
     }
     //todo java bean = metodos get e set, construtor, metodos privados
     
@@ -47,8 +52,39 @@ public class BandeiraMB implements Serializable{
             return "list.xhtml?faces-redirect=true";      
     }else{
             return null;
+    }}
+    
+    public void recuperarBandeira(){
+         HttpServletRequest servReq = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+         HttpSession session = servReq.getSession(true);
+         this.bandeira = (Bandeira) session.getAttribute("bandeira");
+     }
+    
+    public String carregarDados(Bandeira bandeira){
+        HttpServletRequest servReq = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        HttpSession session = servReq.getSession(true);
+        session.getAttribute("bandeira");
+        return "alter.xhtml?faces-redirect=true"; 
     }
-}
+    
+    public String excluir (Bandeira bandeira){
+       BandeiraService service = new BandeiraService();
+        if(service.excluir(bandeira)){
+            return "list.xhtml?faces-redirect=true";      
+    }else{
+            return null;
+    }  
+    }
+        
+    
+  public String alterar(){
+        BandeiraService service = new BandeiraService();
+        if(service.alterar(bandeira)){
+            return "list.xhtml?faces-redirect=true";      
+    }else{
+            return null;
+    }
+        }
         
     public String cancelar(){
         return "list.xhtml?faces-redirect=true"; 
@@ -62,4 +98,11 @@ public class BandeiraMB implements Serializable{
         this.bandeiras = bandeiras;
     }
     
+    public Bandeira getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(Bandeira bandeira) {
+        this.bandeira = bandeira;
+    }
 }
